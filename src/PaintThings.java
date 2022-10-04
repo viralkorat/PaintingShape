1eb5739b47418c9fb60274d997b781ea1d870911
vkorat <vkorat@CATOR-VKORAT1.armstrong.local>
2022-10-04 13:26:09 -0400

    round off to 2 decimal value

--- src/PaintThings.java
+++ src/PaintThings.java
@@ -8,19 +8,19 @@ public class PaintThings {
 		Paint p1 = new Paint();
 		double finalSphereAmount = p1.amount(sphereArea);
 		System.out.println(sp.toString());
-		System.out.println("Sphere Paint Amount = " + finalSphereAmount);
+		System.out.println("Sphere Paint Amount = " + String.format("%.2f", finalSphereAmount));
 
 		Rectangle rec = new Rectangle(20, 35);
 		double recArea = rec.area();
 		double finalRecAmount = p1.amount(recArea);
 		System.out.println(rec.toString());
-		System.out.println("Rectangle Paint Amount = " + finalRecAmount);
+		System.out.println("Rectangle Paint Amount = " + String.format("%.2f", finalRecAmount));
 
 		Cylinder cyl = new Cylinder(10, 30);
 		double cylArea = cyl.area();
 		double finalCylArea = p1.amount(cylArea);
 		System.out.println(cyl.toString());
-		System.out.println("Cylinder Paint Amount = " + finalCylArea);
+		System.out.println("Cylinder Paint Amount = " + String.format("%.2f", finalCylArea));
 
 	}
 
